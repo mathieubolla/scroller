fbd94917baaeda89eaadea54d828575e1a0ee9d2
Mathieu Bolla <mbolla@splayce.com>
2012-03-15 18:33:40 +0100

    Cleaner code

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import os
 import itertools
-import json
+
 
 def inputdirectory():
 	import sys
@@ -28,20 +28,25 @@ def convertJpeg(jpeg):
 	import subprocess
 	subprocess.check_call("gm convert " + inputdirectory() + "/" + jpeg + " -thumbnail 25600@ " + inputdirectory() + "/" + toPng(jpeg), shell=True)
 
-files = os.listdir(inputdirectory())
-jpegs = [f for f in files if f.endswith(".jpg")]
+def outputList(jpegs, groupName, id):
+	import json
+	json.dump(
+		{
+			"data": [format(jpeg) for jpeg in jpegs],
+			"group": groupName
+		},
+		writeTo(id)
+	)
 
-jpegs = sorted(jpegs, key=date)
+jpegs = sorted([f for f in os.listdir(inputdirectory()) if f.endswith(".jpg")], key=date)
 for jpeg in jpegs:
 	convertJpeg(jpeg)
 
-count = 0
+count, image_count = 0, 0
 for group, grouped_jpegs in itertools.groupby(jpegs, date):
-	json.dump(
-		{
-			"data": [format(jpeg) for jpeg in list(grouped_jpegs)],
-			"group": group
-		},
-		writeTo(count)
-	)
+	grouped_jpegs_list = list(grouped_jpegs)
+	outputList(grouped_jpegs_list, group, count)
 	count = count + 1
+	image_count = image_count + len(grouped_jpegs_list)
+	if (count % 10) == 0:
+		print "Processed "+str(count)+" groups with "+str(image_count)+" images."
