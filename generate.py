84e9e0b60b662bace2fbf277227d8b7fdf7e4ec7
Mathieu Bolla <mbolla@splayce.com>
2012-03-15 18:15:41 +0100

    Add conversion to jpg in the python script, remove shell version

--- generate.py
+++ generate.py
@@ -23,11 +23,17 @@ def writeTo(counter):
 	import io
 	counter = counter+1
 	return io.FileIO(outputdirectory() + "/archive" + str(counter) + ".json", "w")
+	
+def convertJpeg(jpeg):
+	import subprocess
+	subprocess.check_call("gm convert " + inputdirectory() + "/" + jpeg + " -thumbnail 25600@ " + inputdirectory() + "/" + toPng(jpeg), shell=True)
 
 files = os.listdir(inputdirectory())
 jpegs = [f for f in files if f.endswith(".jpg")]
 
 jpegs = sorted(jpegs, key=date)
+for jpeg in jpegs:
+	convertJpeg(jpeg)
 
 count = 0
 for group, grouped_jpegs in itertools.groupby(jpegs, date):
--- thumbify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-filename=$1
-gm convert $filename -thumbnail 25600@ ${filename/.jpg/.png}
